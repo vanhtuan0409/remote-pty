1f2529d13109b093d3e33cb422ab4d51a960029b
Tuan Vuong <vanhtuan0409@gmail.com>
2020-09-14 14:20:02 +0700

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/vanhtuan0409/remote-pty
+
+go 1.15
+
+require (
+	github.com/creack/pty v1.1.11
+	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009 // indirect
+)
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/creack/pty"
+)
+
+func main() {
+	command := exec.Command("zsh")
+	ptmx, err := pty.Start(command)
+	if err != nil {
+		panic(err)
+	}
+	defer ptmx.Close()
+
+	// handle resize terminal events
+	resizeCh := make(chan os.Signal, 1)
+	signal.Notify(resizeCh, syscall.SIGWINCH)
+	go func() {
+		for range resizeCh {
+			if err := pty.InheritSize(os.Stdin, ptmx); err != nil {
+				log.Printf("Failed to resize PTY. ERR: %v", err)
+			}
+		}
+	}()
+	resizeCh <- syscall.SIGWINCH //initial resize
+
+	buf := &bytes.Buffer{}
+	writer := io.MultiWriter(os.Stdout, buf)
+
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Printf("Cannot accpet connection. ERR: %v", err)
+				continue
+			}
+			go handleConn(conn, buf)
+		}
+	}()
+
+	go io.Copy(ptmx, os.Stdin)
+	io.Copy(writer, ptmx)
+}
+
+func handleConn(c net.Conn, input io.Reader) {
+	defer c.Close()
+	io.Copy(c, input)
+}
